35138af12eddcae0ff5f94caf1df3198f9f0a414
Alec <vanoosterwijckalec@hotmail.com>
2022-04-28 15:13:30 +0200

    Api maken

---
+++ EncryptieAPI/EncryptieAPI/Encryptie.cs
@@ -0,0 +1,8 @@
+﻿namespace EncryptieAPI
+{
+    public class Encryptie
+    {
+        public string Key { get; set; } = string.Empty;
+        public string bericht { get; set; } = string.Empty;
+    }
+}
---
+++ EncryptieAPI/EncryptieAPI/Models/Bericht.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EncryptieAPI.Models
+{
+    public class Bericht
+    {
+        [Required]
+        public string Ebericht { get; set; } = string.Empty;
+        public int id { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
