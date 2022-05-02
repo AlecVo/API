7fe24f8237a7417c65b8c517d88bf9a0c94cae3b
Alec <vanoosterwijckalec@hotmail.com>
2022-05-02 16:21:09 +0200

    verwijderen onnodige dingen

--- EncryptieAPI/EncryptieAPI/Controllers/EncryptieController.cs
+++ EncryptieAPI/EncryptieAPI/Controllers/EncryptieController.cs
@@ -1,4 +1,5 @@
-﻿using EncryptieAPI.Models;
+﻿using EncryptieAPI.Data;
+using EncryptieAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,48 +9,32 @@ namespace EncryptieAPI.Controllers
     [ApiController]
     public class EncryptieController : ControllerBase
     {
-        private static List<Bericht> berichten = new List<Bericht>
+        private readonly EncryptieDbContext dbContext;
+        public EncryptieController(EncryptieDbContext dbContext)
         {
-            new Bericht { id = 1, date = DateTime.Now, Ebericht = "Hallo"},
-            new Bericht { id = 2, date = DateTime.Now, Ebericht = "iets"},
-            new Bericht { id = 3, date = DateTime.Now, Ebericht = "Test"},
-        };
-
-        [HttpGet("BerichtLezen")]
-        public async Task<ActionResult<List<Bericht>>> get()
-        {
-            return Ok(berichten);
-        }
-        //laat de berichten zien
-        //TODO: database koppelen en zien hoe dat de api het juiste bericht ophaalt.
-        [HttpPost("BerichtToevoegen")]
-
-        public async Task<ActionResult<List<Bericht>>> AddPrivateKey(Bericht bericht)
-        {
-            berichten.Add(bericht);
-            return Ok(berichten);
-        }
-        //Voegt een bericht toe 
-        //TODO: Database koppelen zodat het berichten gaat opslaan in de databank
-        [HttpGet("LinkLezen")]
-        public async Task<ActionResult<List<PrivateKey>>> get(string link)
-        {
-            return Ok(berichten);
+            this.dbContext = dbContext;
         }
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<List<Bericht>>> Delete(int id)
+        [HttpPost("Ophalen bericht")]
+
+        public ActionResult<Berichten> OphalenEncryptedMessage(Guid Id)
         {
-            var bericht = berichten.Find(b => b.id == id);//Bericht is bericht waar bericht id hetzelfde is als het ingegeven id
-            if (bericht == null)
+            Berichten berichten;
+            //Ophalen bericht
+            //Gegevens van het bericht invullen
+            berichten.(b => b.id == Id && b.isVervalt = false)
             {
-                return BadRequest("Hero met dit id is niet gevonden");
+
             }
-            berichten.Remove(bericht);
-            return Ok(berichten);
+            bericht.id = Id;
+            bericht.aanmaakDatum = DateTime.Now;
+            bericht.encryptedBericht =
+
+
+
+
         }
-        //gaat berichten verwijderen op id.
-        //TODO: Berichten verwijderen nadat een tijdsbepaling overschreden is => bij een bericht dateTime toevoegen en een verwijder datum.
-        //daarna op basis van tijd en het bericht id word het verwijdert van de database.
+
     }
+
 }
