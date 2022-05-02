57ea922792c2c1d14b8f1f54eac5e3df08c1867b
Alec <vanoosterwijckalec@hotmail.com>
2022-05-02 16:21:44 +0200

    verwijderen onndogie items

--- EncryptieAPI/EncryptieAPI/Encryptie.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EncryptieAPI
-{
-    public class Encryptie
-    {
-        public string Key { get; set; } = string.Empty;
-        public string bericht { get; set; } = string.Empty;
-    }
-}
