c0a1268881700d8000efa6e370a604c19869483d
Alec <vanoosterwijckalec@hotmail.com>
2022-05-02 14:55:41 +0200

    Aanpassen bericht

---
+++ EncryptieAPI/EncryptieAPI/Models/Berichten.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EncryptieAPI.Models
+{
+    public class Berichten 
+    {
+        public int id { get; set; }
+        public string encryptedBericht { get; set; }
+        public DateTime vervalDatum { get; set; } = 7;
+        public DateTime aanmaakDatum { get; set; }
+
+    }
+}
