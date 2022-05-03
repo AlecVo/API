40d48b13b98b37f3229717566a7a696818a9eb22
Alec <vanoosterwijckalec@hotmail.com>
2022-05-03 10:57:12 +0200

    Opslaan van bericht zonder database

--- EncryptieAPI/EncryptieAPI/Controllers/EncryptieController.cs
+++ EncryptieAPI/EncryptieAPI/Controllers/EncryptieController.cs
@@ -9,19 +9,31 @@ namespace EncryptieAPI.Controllers
     [ApiController]
     public class EncryptieController : ControllerBase
     {
+        private static List<Bericht> berichten = new List<Bericht>
+        {
+            new Bericht
+            {
+                id = Guid.NewGuid(),
+                encryptedBericht = "def1ert4",
+                aanmaakDatum = DateTime.Now,
+                vervalDatum = 44,
+                isVervalt = false
+
+            }
+        };
 
         [HttpPost("Opslaan bericht")]
 
-        public async Task<ActionResult<List<Bericht>>> AddBericht()
+        public async Task<ActionResult<List<Bericht>>> AddBericht(Bericht bericht)
         {
-            var bericht = new List<Bericht>
-            { 
-                new Bericht
-                {
-
-                }
-            };
+            berichten.Add(bericht);
             return Ok(bericht);
+        }
+
+        [HttpGet("Testen code")]
+        public async Task<ActionResult<List<Bericht>>> Get()
+        {
+            return Ok(berichten);
         }
 
     }
