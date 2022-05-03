01434cb03b28c10352d68fc33d36c060beb4855c
Alec <vanoosterwijckalec@hotmail.com>
2022-05-03 10:57:27 +0200

    Toevoegen van db context in een data map

--- EncryptieAPI/EncryptieAPI/Data/EncryptieDbContext.cs
+++ EncryptieAPI/EncryptieAPI/Data/EncryptieDbContext.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EncryptieAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EncryptieAPI.Data
 {
     public class EncryptieDbContext : DbContext
     {
-        //public EncryptieDbContext(DbContextOptions options) : base(options)
-        //{
-        //    Public Bericht {
+        public EncryptieDbContext(DbContextOptions options) : base(options) { }
 
-        //    }
-        //}
+        public DbSet<Bericht> Berichten { get; set; }
     }
 }
